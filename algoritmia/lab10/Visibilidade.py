6c672a609a4aecccaa463777aed97b7c6c601302
Rafael Correia <rafael.correia2020@outlook.com>
2025-02-17 10:17:34 +0000

    lab10

---
+++ algoritmia/lab10/Visibilidade.py
@@ -0,0 +1,24 @@
+'''
+
+
+
+'''
+
+
+x = 98
+
+def procedimento1():
+    print(x)
+
+    def procedimento2():
+        print(x)
+
+        procedimento1()
+        procedimento2(
+            print(x)
+        )
+
+
+
+
+
